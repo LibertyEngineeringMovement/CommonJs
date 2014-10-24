91c34fff6310e7fab093f33215a1a3b5fab997e1
Brian Webb <BrianMinister@users.noreply.github.com>
2014-10-24 13:55:40 -0700

    Create Common.js
    This is my common js file. It is a collection of prototypes from Mozilla, and a couple of helpers from StackOverflow.
    
    If you are going to comment about my license being wrong for this, then please note that I am still compiling all of the licenses that I need, and will add them to the code. The good news is that off of the licenses that I have pulled this from are from open source lic. sites.

---
+++ Common.js
@@ -0,0 +1,535 @@
+/*ARRAY OBJECTS*/
+/*Array.filter()*/
+if (!Array.prototype.filter) {
+    Array.prototype.filter = function (fun /*, thisArg */) {
+        "use strict";
+
+        if (this === void 0 || this === null)
+            throw new TypeError();
+
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (typeof fun !== "function")
+            throw new TypeError();
+
+        var res = [];
+        var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+        for (var i = 0; i < len; i++) {
+            if (i in t) {
+                var val = t[i];
+
+                // NOTE: Technically this should Object.defineProperty at
+                //       the next index, as push can be affected by
+                //       properties on Object.prototype and Array.prototype.
+                //       But that method's new, and collisions should be
+                //       rare, so use the more-compatible alternative.
+                if (fun.call(thisArg, val, i, t))
+                    res.push(val);
+            }
+        }
+
+        return res;
+    };
+}
+
+/*Array.find()*/
+if (!Array.prototype.find) {
+    Object.defineProperty(Array.prototype, 'find', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function (predicate) {
+            if (this == null) {
+                throw new TypeError('Array.prototype.find called on null or undefined');
+            }
+            if (typeof predicate !== 'function') {
+                throw new TypeError('predicate must be a function');
+            }
+            var list = Object(this);
+            var length = list.length >>> 0;
+            var thisArg = arguments[1];
+            var value;
+
+            for (var i = 0; i < length; i++) {
+                if (i in list) {
+                    value = list[i];
+                    if (predicate.call(thisArg, value, i, list)) {
+                        return value;
+                    }
+                }
+            }
+            return undefined;
+        }
+    });
+}
+
+/*Array.find()*/
+if (!Array.prototype.findOrNull) {
+    Object.defineProperty(Array.prototype, 'findOrNull', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function (predicate) {
+            if (this == null) {
+                throw new TypeError('Array.prototype.findOrNull called on null or undefined');
+            }
+            if (typeof predicate !== 'function') {
+                throw new TypeError('predicate must be a function');
+            }
+            var list = Object(this);
+            var length = list.length >>> 0;
+            var thisNullArg = arguments[1];
+            var thisArg = arguments[2];
+            var value;
+
+            console.log("findOrNull - arguments", arguments);
+
+            for (var i = 0; i < length; i++) {
+                if (i in list) {
+                    value = list[i];
+                    if (predicate.call(thisArg, value, i, list)) {
+                        return value;
+                    }
+                }
+            }
+            return thisNullArg;
+        }
+    });
+}
+
+
+/*Array.findIndex()*/
+if (!Array.prototype.findIndex) {
+    Object.defineProperty(Array.prototype, 'findIndex', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function (predicate) {
+            if (this == null) {
+                throw new TypeError('Array.prototype.find called on null or undefined');
+            }
+            if (typeof predicate !== 'function') {
+                throw new TypeError('predicate must be a function');
+            }
+            var list = Object(this);
+            var length = list.length >>> 0;
+            var thisArg = arguments[1];
+            var value;
+
+            for (var i = 0; i < length; i++) {
+                if (i in list) {
+                    value = list[i];
+                    if (predicate.call(thisArg, value, i, list)) {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+    });
+}
+
+/*STRING OBJECTS*/
+/*String.fromCodePoint()*/
+if (!String.fromCodePoint) {
+    (function () {
+        var defineProperty = (function () {
+            // IE 8 only supports `Object.defineProperty` on DOM elements
+            try {
+                var object = {};
+                var $defineProperty = Object.defineProperty;
+                var result = $defineProperty(object, object, object) && $defineProperty;
+            } catch (error) { }
+            return result;
+        }());
+        var stringFromCharCode = String.fromCharCode;
+        var floor = Math.floor;
+        var fromCodePoint = function () {
+            var MAX_SIZE = 0x4000;
+            var codeUnits = [];
+            var highSurrogate;
+            var lowSurrogate;
+            var index = -1;
+            var length = arguments.length;
+            if (!length) {
+                return '';
+            }
+            var result = '';
+            while (++index < length) {
+                var codePoint = Number(arguments[index]);
+                if (
+                    !isFinite(codePoint) || // `NaN`, `+Infinity`, or `-Infinity`
+                    codePoint < 0 || // not a valid Unicode code point
+                    codePoint > 0x10FFFF || // not a valid Unicode code point
+                    floor(codePoint) != codePoint // not an integer
+                ) {
+                    throw RangeError('Invalid code point: ' + codePoint);
+                }
+                if (codePoint <= 0xFFFF) { // BMP code point
+                    codeUnits.push(codePoint);
+                } else { // Astral code point; split in surrogate halves
+                    // http://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+                    codePoint -= 0x10000;
+                    highSurrogate = (codePoint >> 10) + 0xD800;
+                    lowSurrogate = (codePoint % 0x400) + 0xDC00;
+                    codeUnits.push(highSurrogate, lowSurrogate);
+                }
+                if (index + 1 == length || codeUnits.length > MAX_SIZE) {
+                    result += stringFromCharCode.apply(null, codeUnits);
+                    codeUnits.length = 0;
+                }
+            }
+            return result;
+        };
+        if (defineProperty) {
+            defineProperty(String, 'fromCodePoint', {
+                'value': fromCodePoint,
+                'configurable': true,
+                'writable': true
+            });
+        } else {
+            String.fromCodePoint = fromCodePoint;
+        }
+    }());
+}
+
+/*String.codePointAt()*/
+if (!String.prototype.codePointAt) {
+    (function () {
+        'use strict'; // needed to support `apply`/`call` with `undefined`/`null`
+        var codePointAt = function (position) {
+            if (this == null) {
+                throw TypeError();
+            }
+            var string = String(this);
+            var size = string.length;
+            // `ToInteger`
+            var index = position ? Number(position) : 0;
+            if (index != index) { // better `isNaN`
+                index = 0;
+            }
+            // Account for out-of-bounds indices:
+            if (index < 0 || index >= size) {
+                return undefined;
+            }
+            // Get the first code unit
+            var first = string.charCodeAt(index);
+            var second;
+            if ( // check if it’s the start of a surrogate pair
+                first >= 0xD800 && first <= 0xDBFF && // high surrogate
+                size > index + 1 // there is a next code unit
+            ) {
+                second = string.charCodeAt(index + 1);
+                if (second >= 0xDC00 && second <= 0xDFFF) { // low surrogate
+                    // http://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+                    return (first - 0xD800) * 0x400 + second - 0xDC00 + 0x10000;
+                }
+            }
+            return first;
+        };
+        if (Object.defineProperty) {
+            Object.defineProperty(String.prototype, 'codePointAt', {
+                'value': codePointAt,
+                'configurable': true,
+                'writable': true
+            });
+        } else {
+            String.prototype.codePointAt = codePointAt;
+        }
+    }());
+}
+
+/*String.contains()*/
+if (!String.prototype.contains) {
+    String.prototype.contains = function () {
+        return String.prototype.indexOf.apply(this, arguments) !== -1;
+    };
+}
+
+/*String.endsWith()*/
+if (!String.prototype.endsWith) {
+    (function () {
+        'use strict'; // needed to support `apply`/`call` with `undefined`/`null`
+        var defineProperty = (function () {
+            // IE 8 only supports `Object.defineProperty` on DOM elements
+            try {
+                var object = {};
+                var $defineProperty = Object.defineProperty;
+                var result = $defineProperty(object, object, object) && $defineProperty;
+            } catch (error) { }
+            return result;
+        }());
+        var toString = {}.toString;
+        var endsWith = function (search) {
+            if (this == null) {
+                throw TypeError();
+            }
+            var string = String(this);
+            if (search && toString.call(search) == '[object RegExp]') {
+                throw TypeError();
+            }
+            var stringLength = string.length;
+            var searchString = String(search);
+            var searchLength = searchString.length;
+            var pos = stringLength;
+            if (arguments.length > 1) {
+                var position = arguments[1];
+                if (position !== undefined) {
+                    // `ToInteger`
+                    pos = position ? Number(position) : 0;
+                    if (pos != pos) { // better `isNaN`
+                        pos = 0;
+                    }
+                }
+            }
+            var end = Math.min(Math.max(pos, 0), stringLength);
+            var start = end - searchLength;
+            if (start < 0) {
+                return false;
+            }
+            var index = -1;
+            while (++index < searchLength) {
+                if (string.charCodeAt(start + index) != searchString.charCodeAt(index)) {
+                    return false;
+                }
+            }
+            return true;
+        };
+        if (defineProperty) {
+            defineProperty(String.prototype, 'endsWith', {
+                'value': endsWith,
+                'configurable': true,
+                'writable': true
+            });
+        } else {
+            String.prototype.endsWith = endsWith;
+        }
+    }());
+}
+
+/*String.startsWith()*/
+if (!String.prototype.startsWith) {
+    (function () {
+        'use strict'; // needed to support `apply`/`call` with `undefined`/`null`
+        var defineProperty = (function () {
+            // IE 8 only supports `Object.defineProperty` on DOM elements
+            try {
+                var object = {};
+                var $defineProperty = Object.defineProperty;
+                var result = $defineProperty(object, object, object) && $defineProperty;
+            } catch (error) { }
+            return result;
+        }());
+        var toString = {}.toString;
+        var startsWith = function (search) {
+            if (this == null) {
+                throw TypeError();
+            }
+            var string = String(this);
+            if (search && toString.call(search) == '[object RegExp]') {
+                throw TypeError();
+            }
+            var stringLength = string.length;
+            var searchString = String(search);
+            var searchLength = searchString.length;
+            var position = arguments.length > 1 ? arguments[1] : undefined;
+            // `ToInteger`
+            var pos = position ? Number(position) : 0;
+            if (pos != pos) { // better `isNaN`
+                pos = 0;
+            }
+            var start = Math.min(Math.max(pos, 0), stringLength);
+            // Avoid the `indexOf` call if no match is possible
+            if (searchLength + start > stringLength) {
+                return false;
+            }
+            var index = -1;
+            while (++index < searchLength) {
+                if (string.charCodeAt(start + index) != searchString.charCodeAt(index)) {
+                    return false;
+                }
+            }
+            return true;
+        };
+        if (defineProperty) {
+            defineProperty(String.prototype, 'startsWith', {
+                'value': startsWith,
+                'configurable': true,
+                'writable': true
+            });
+        } else {
+            String.prototype.startsWith = startsWith;
+        }
+    }());
+}
+
+/*OBJECTS OBJECT*/
+/*Object.is()*/
+if (!Object.is) {
+    Object.is = function (v1, v2) {
+        if (v1 === 0 && v2 === 0) {
+            return 1 / v1 === 1 / v2;
+        }
+        if (v1 !== v1) {
+            return v2 !== v2;
+        }
+        return v1 === v2;
+    };
+}
+
+/*Object.setPrototypeOf()*/
+if (!Object.setPrototypeOf) {
+    Object.setPrototypeOf = function (obj, proto) {
+        obj.__proto__ = proto;
+        return obj;
+    }
+}
+
+/*Number.parseFloat()*/
+if (!Number.prototype.parseFloat) {
+    Number.prototype.parseFloat = function (value) {
+        if (/^(\-|\+)?([0-9]+(\.[0-9]+)?|Infinity)$/.test(value)) {
+            return Number(value);
+        }
+        else {
+            return NaN;
+        }
+    };
+}
+
+
+/*clone(obj)*/
+function clone(obj) {
+    //console.log('Cloning');
+    if (!obj) { return obj; } 
+
+    var types = [Number, String, Boolean],
+        result;
+
+    types.forEach(function (type) {
+        if (obj instanceof type) {
+            result = type(obj);
+        }
+    });
+
+    if (typeof result == "undefined") {
+
+        if (Object.prototype.toString.call(obj) === "[object Array]") {
+
+            result = [];
+            obj.forEach(function (child, index, array) {
+                result[index] = clone(child);
+            });
+
+        }
+        else if (typeof obj == "object") {
+
+            if (obj.nodeType && typeof obj.cloneNode == "function") {
+                var result = obj.cloneNode(true);
+            }
+            else if (!obj.prototype) {
+                if (obj instanceof Date) {
+                    result = new Date(obj);
+                }
+                else {
+                    result = {};
+                    for (var i in obj) {
+                        result[i] = clone(obj[i]);
+                    }
+                }
+            }
+            else {
+                if (false && obj.constructor) {
+                    result = new obj.constructor();
+                }
+                else {
+                    result = obj;
+                }
+            }
+
+        }
+        else {
+            result = obj;
+        }
+
+    }
+    if (result.$$hashKey) {
+        delete result.$$hashKey;
+    }
+    //console.log('Cloned');
+    return result;
+}
+
+/*Get the GUIDs from the server.*/
+var guidCount = 0;
+function newGuid(postback, url, count) {
+    if (url === undefined || url === null || ulr.length === 0) {
+        url = '../api/Guid/';
+    }
+
+    if (count === undefined || count === null || count === '') {
+        count = 1;
+    }
+
+    if (url[url.length - 1] !== '/') {
+        url = url + '/' + count.toString() + '/';
+    }
+    else {
+        url = url + count.toString() + '/';
+    }
+    url = url + "?" + (Date.now() + guidCount).toString();
+    guidCount++;
+
+    $.getJSON(url, null).done(postback);
+}
+
+/*JSON*/
+if (!window.JSON) {
+    window.JSON = {
+        parse: function (sJSON) { return eval("(" + sJSON + ")"); },
+        stringify: function (vContent) {
+            if (vContent instanceof Object) {
+                var sOutput = "";
+                if (vContent.constructor === Array) {
+                    for (var nId = 0; nId < vContent.length; sOutput += this.stringify(vContent[nId]) + ",", nId++);
+                    return "[" + sOutput.substr(0, sOutput.length - 1) + "]";
+                }
+                if (vContent.toString !== Object.prototype.toString) {
+                    return "\"" + vContent.toString().replace(/"/g, "\\$&") + "\"";
+                }
+                for (var sProp in vContent) {
+                    sOutput += "\"" + sProp.replace(/"/g, "\\$&") + "\":" + this.stringify(vContent[sProp]) + ",";
+                }
+                return "{" + sOutput.substr(0, sOutput.length - 1) + "}";
+            }
+            return typeof vContent === "string" ? "\"" + vContent.replace(/"/g, "\\$&") + "\"" : String(vContent);
+        }
+    };
+}
+
+// Adds the .NET String.Format() function to JavaScript
+//This is an alternative to http://ajaxcontroltoolkit.codeplex.com/SourceControl/latest#Client/MicrosoftAjax/Extensions/String.js
+if (!String.prototype.format) {
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined'
+              ? args[number]
+              : match
+            ;
+        });
+    };
+}
+
+/*Angular Specific Digest() Command*/
+function Digest($scope) {
+    //this may not be future proof, if it isn't then you may have to try $timeout(function(){//do something})
+    if (!$scope.$$phase) {
+        try {
+            //using digest over apply, because apply fires at root, and I generally don't need root.
+            $scope.$digest();
+        }
+        catch (e) { }
+    }
+    //console.log('$scope snapshot:', $scope);
+}
